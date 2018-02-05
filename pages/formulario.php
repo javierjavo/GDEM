9aca2b7e97e462fb28766f91d2233da1371621b2
Chuck <so.ulreaver@hotmail.com>
2018-02-05 17:01:05 -0600

    Registro completo
    se agrego el registro de usuario, se estandariza alertas con SESSION, se comprimen imagenes y se borran archivos no estandarizados

---
+++ pages/formulario.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<?php
+    include "../servicios/lenguaje.php";
+    initStyles();
+?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width = device-width, initial-scale=1, shrink-to-fit = no" >
+        <title>Home GDEM</title>
+         <!-- Bootstrap CSS -->
+    </head>
+    <body class="fondoHome">
+        <header  role="banner">
+            <?php
+                include"../objetos/header.php";
+            ?>
+        </header>
+   	   	<!--Contenido de la pagina-->
+	<main class="contenido" role="main">
+		<section class="container" id="languajes" >
+            <div class="row align-items-center bg-light"  style="min-height: 675px;">
+                <form action="../servicios/insertar.php" class="login" method="post">
+                    <fieldset>
+                      <legend>Introduce tu informacion aqui / Enter your information here</legend>
+                       <label for="nombre"> Nombre o Pseudonimo / Name or Nickname:
+                        <input type="text" name="nombre" id="nombre" placeholder="Nombre / Name" autocomplete="on" required>
+                        </label>
+                        <br>
+                        <label for="edad"> Edad / Age:
+                          <input type="number" name="edad" id="edad" placeholder="Edad / Age" autocomplete="on" required min="0"/>
+                        </label>
+                        <br>
+                        <label for="">Residencia / Place of residence</label>
+                          <select class="residencia" name="residencia">
+                            <option value="usa">United States of America</option>
+                            <option value="mex" selected>Mexico</option>       
+                          </select>
+                        <br>
+                        <label for="">Tipo de usuario / Type of user</label>
+                            <select class="tipousuario" name="tipousuario">
+                                <option value="maestro">Maestro </option>
+                                <option value="alumno" selected>Alumno</option>         
+                            </select>
+                        <br>
+                        <label for="">Ultima escolaridad cursada / Last grade taken: (México)</label>
+                          <select class="residencia" name="escolaridad">
+                            <option value="kinderGarden">Kinder Garden</option>
+                            <option value="elementary" selected>Elementary School</option>       
+                            <option value="middle" selected>Middle School</option>       
+                            <option value="high" selected>High School</option> 
+                            <option value="primaria">Educación basica</option>
+                            <option value="secundaria">Educación Media</option>
+                            <option value="universidad">Educación Media Superior</option>
+                          </select>
+                        <br>
+                        <label for="">
+                          Contraseña / Password: <input type="password" name="clave1" size="20" placeholder="ingresar password"> 
+                        </label>
+                        <br> 
+                        <label for="">
+                          Repite la contraseña / Repeat password: <input type="password" name="clave2" size="20" placeholder="repetir password"> 
+                        </label> <br>
+                        <input class="btn btn-primary" name="submit" type="submit" id="boton" value="Registrar / Register" class="boton" onClick="comprobarClave()"/>
+                        <button class="btn btn-secondary" type="reset" >Limpiar formulario / Clean form</button>       
+                    </fieldset>
+                </form>
+            </div>
+		</section>
+	</main>
+        <!--Footer de la pagina -->
+    <?php
+        include "../objetos/pie.php";
+    ?>
+</body>
--- servicios/insertar.php
+++ servicios/insertar.php
@@ -1,26 +1,42 @@
 <?php
-if (isset($_POST['nombre']) and isset($_POST['clave1'])){
+include 'conexion.php';
+session_start();
+
+function Verificacion($nombre){
     include 'conexion.php';
+    $consulta_a_la_base=mysqli_query($conexion,'select IDLogin from login where user="'.$nombre.'"');
+    $recoger_dato=mysqli_fetch_assoc($consulta_a_la_base);
+    return $recoger_dato['IDLogin'];
+}
+
+if (isset($_POST['nombre']) and isset($_POST['clave1'])){
     $idioma="";
     $nombredeusuario=mysqli_real_escape_string($conexion,$_POST['nombre']);
     $password=password_hash($_POST['clave1'],PASSWORD_DEFAULT);
     $edad=mysqli_real_escape_string($conexion,$_POST['edad']);
     $tipousuario=mysqli_real_escape_string($conexion,$_POST['tipousuario']);
     $residencia=mysqli_real_escape_string($conexion,$_POST['residencia']);
+    $escolaridad=mysqli_real_escape_string($conexion,$_POST['escolaridad']);
     
     if ($residencia=="usa"){
-        $escolaridad=mysqli_real_escape_string($conexion,$_POST['escolaridad1']);
-        $idioma="eng";
+        $idioma="en";
     }else{
-        $escolaridad=mysqli_real_escape_string($conexion,$_POST['escolaridad2']);
-        $idioma="esp";
+        $idioma="es";
     }
-    
-    $insertar=mysqli_query($conexion, 'insert into login(user,pass) values ("'.$nombredeusuario.'","'.$password.'")') or die ('No se puede registrar<br>'.mysqli_error($conexion));
-    $insertar1=mysqli_query($conexion, 'insert into personalinfo(nombre,edad,escolaridad,tipousuario,pais,idioma) values ("'.$nombredeusuario.'","'.$edad.'","'.$escolaridad.'","'.$tipousuario.'","'.$escolaridad.'","'.$residencia.'","'.$idioma.'")') or die ('No se puede registrar<br>'.mysqli_error($conexion));
-    mysqli_close($conexion);
-    header('location: www.google.com');
+    $id=Verificacion($nombredeusuario);
+    if($id==null){
+        $insertar=mysqli_query($conexion, 'insert into login(user,pass) values ("'.$nombredeusuario.'","'.$password.'")') or die ('No se puede registrar<br>'.mysqli_error($conexion));
+        $id=Verificacion($nombredeusuario);
+        $insertar1=mysqli_query($conexion, "INSERT INTO `personalinfo`(`IDLogin`, `nombre`, `edad`, `tipousuario`, `escolaridad`, `pais`, `idioma`) VALUES('$id','$nombredeusuario','$edad','$tipousuario','$escolaridad','$residencia','$idioma')")or die ('No se puede registrar<br>'.mysqli_error($conexion));
+        mysqli_close($conexion);
+        $_SESSION['error'] = $nombredeusuario." registrado con exito";
+    }else{
+        //respondes un error de cuenta existente\
+        $_SESSION['alert'] = "El usuario ya esta en uso intenta usar otro, puede ser tu correo electronico";
+    }
 }else{
-    header('location: www.youtube.com');
+    if(!isset($_SERVER['HTTP_REFERER']))
+        header("Location: ../index.php");
 }
+header("Location:".$_SERVER['HTTP_REFERER']);
 ?>
