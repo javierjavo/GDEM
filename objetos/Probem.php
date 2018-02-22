6b4e9bd6acd42d9a14a9eb9cb43d8b21bab64dc1
Nacho <ignacio.sa1211@gmail.com>
2018-02-21 22:11:27 -0600

    Los cambios que no subí jeje
    Cambios en el index y creacion de paginas con contenido dinamico

---
+++ objetos/Probem.php
@@ -0,0 +1,53 @@
+<?php
+$homeRute = "/GDEM";
+include($_SERVER['DOCUMENT_ROOT'].'/GDEM/servicios/conexion.php');
+session_start();
+$langProbem = [];
+$resultado = $conexion->query("SELECT ".$_SESSION['lang']." FROM lenguaje WHERE page='pie'");
+$resultado->data_seek(0);
+while ($fila = $resultado->fetch_assoc()) {
+    $langProbem[]= $fila[$_SESSION['lang']];
+}
+echo'
+               <div class="col-md-6">
+                   <form action="../servicios/insertar.php" class="login" method="post">
+                    <fieldset>
+                      <legend>Imagen y Texto</legend>
+                       <label for="nombre"> Nombre o Pseudonimo / Name or Nickname:
+                        <input type="text" name="nombre" id="nombre" placeholder="Nombre / Name" autocomplete="on" required>
+                        </label>
+                        <br>
+                        <label for="estado"> Estado de residencia/Current state of residence:
+                        <input type="text" name="estado" id="estado" placeholder="Estado / State" autocomplete="on" required>
+                        </label>
+                        <br>
+                        <label for="comentarios"> Comentarios/Comments:
+                        <input type="text" name="comentarios" id="comentarios" placeholder="Comentarios / Commentarios" autocomplete="on" required>
+                        </label>
+                        <br>
+                        <input class="btn btn-primary" name="submit" type="submit" id="boton" value="Enviar Commentarios / Send comments" class="boton" onClick=""/>
+                    </fieldset>
+                </form>
+               </div>
+               <div class="col-md-6">
+                   <form action="../servicios/insertar.php" class="login" method="post">
+                    <fieldset>
+                      <legend>*¿Que es PROBEM?*</legend>
+                       <label for="nombre"> Nombre o Pseudonimo / Name or Nickname:
+                        <input type="text" name="nombre" id="nombre" placeholder="Nombre / Name" autocomplete="on" required>
+                        </label>
+                        <br>
+                        <label for="estado"> Estado de residencia/Current state of residence:
+                        <input type="text" name="estado" id="estado" placeholder="Estado / State" autocomplete="on" required>
+                        </label>
+                        <br>
+                        <label for="comentarios"> Comentarios/Comments:
+                        <input type="text" name="comentarios" id="comentarios" placeholder="Comentarios / Commentarios" autocomplete="on" required>
+                        </label>
+                        <br>
+                        <input class="btn btn-primary" name="submit" type="submit" id="boton" value="Enviar Commentarios / Send comments" class="boton" onClick=""/>
+                    </fieldset>
+                </form>
+               </div>
+    ';
+?>
