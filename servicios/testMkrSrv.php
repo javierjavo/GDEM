d62bb8eeb51285d2a9f6af4718335ea9b65ca9a4
Chuck <so.ulreaver@hotmail.com>
2018-02-24 19:37:18 -0600

    Correccion de TestMkrSrv.php
    Se corrige la insercion a DB

--- servicios/testMkrSrv.php
+++ servicios/testMkrSrv.php
@@ -17,10 +17,19 @@
         return $recoger_dato['IDPregunta'];
     }
 
+    function validarIDRespuesta($ID){
+        include 'conexion.php';
+        $consulta_a_la_base=mysqli_query($conexion,'select IDRespuesta from respuestas where ="'.$IDPrg.'"');
+        $recoger_dato=mysqli_fetch_assoc($consulta_a_la_base);
+        return $recoger_dato['IDRespuesta'];
+    }
+
     $nombre="examen";
     $idioma=$_SESSION['lang'];
     $tipoExamen=mysqli_real_escape_string($conexion,$_POST['tipo']);
     $field_values_array = $_REQUEST['field_name'];
+    $field_values_array2 = $_REQUEST['field_name2'];
+    $field_values_array3 = $_REQUEST['field_name3'];
     
     $idAct=validarIDAct($tipoExamen);
     if($idAct==null){
@@ -29,11 +38,15 @@
         
     $idp=validarIDPregunta($idAct);//ver si existe id de examen y si no, entonces creamos el conjunto de prenguntas
     if(idp==null){
-        foreach($field_values_array as $pregunta){
             $insertar1=mysqli_query($conexion, 'insert into preguntas(IDActividad,textoPregunta) values("'.$idp.'","'.$pregunta.'")') or die ('No se puede registrar<br>'.mysqli_error($conexion));
-        }
+            $idr=validarIDRespuesta($idp);//ver si existe id de la pregunta y si no, entonces creamos el conjunto de respuestas
+        if(idr==null){
+            foreach($field_values_array2 as $respuesta){
+                $insertar2=mysqli_query($conexion, 'insert into preguntas(IDPregunta,textoRespuesta,valor) values("'.$idr.'","'.$respuesta.'")') or die ('No se puede registrar<br>'.mysqli_error($conexion));
+            }
+        }   
     }    
-    
+
     mysqli_close($conexion);
     $_SESSION['error'] ="Examen tipo: "$tipo.", Registrado con exito";
     }else{
