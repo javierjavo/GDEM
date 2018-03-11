7c5371d7696c4652892548e01a094bde235e7a21
Chuck <so.ulreaver@hotmail.com>
2018-03-11 17:49:38 -0600

    Actualizacion de Git
    Se borraron archivos vacios, se eliminan funciones de Index.js que comprobaban links, y se corrobora que no haya fallas

--- js/index.js
+++ js/index.js
@@ -23,33 +23,6 @@ $("#sendInformationLogin").click(function() {
     window.location.href=homeRoute+"/servicios/login.php?usr="+$("#colectUserInfoName").val()+"&pass="+$("#colectUserInfoPass").val();
 });
 
-function redirToPage(url){
-    var request = new XMLHttpRequest();  
-    request.open('GET', url, false);  
-    request.send();
-    // ver estado del request redyStatus
-    /*
-    <script src="https://cdn.jsdelivr.net/npm/vue-resource@1.4.0"></script>
-        {
-      // GET /someUrl
-      this.$http.get('/someUrl').then(response => {
-
-        // get body data
-        this.someData = response.body;
-
-      }, response => {
-        // error callback
-      });
-    }
-    */
-    if (request.status == 404) {  
-        alert("parece que esta no fue encontrada, lo resolveremos lo mas pronto posible");
-    }else{
-        alert("okis");
-        window.location.href = url;
-    }
-}
-
 var contpreg=0;
 var preg = [];
 var resp = [];
