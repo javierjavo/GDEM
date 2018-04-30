c1dee19d9f060fed3a2c2ae1031ead899f953bb1
Chuck <so.ulreaver@hotmail.com>
2018-04-29 22:56:17 -0500

    HTML FIX: AccesoEscuelas
    Se reorganiza el contenido HTML para adaptar a la vista de la db

--- objetos/EstAccesoEscuelas.php
+++ objetos/EstAccesoEscuelas.php
@@ -3,62 +3,39 @@ $homeRute = "/GDEM";
 include($_SERVER['DOCUMENT_ROOT'].'/GDEM/servicios/conexion.php');
 session_start();
 $langCapacitacion = [];
-$resultado = $conexion->query("SELECT ".$_SESSION['lang']." FROM lenguaje WHERE page='capacitacion'");
+$resultado = $conexion->query("SELECT ".$_SESSION['lang']." FROM lenguaje WHERE page='acceso_escuelas'");
 $resultado->data_seek(0);
 while ($fila = $resultado->fetch_assoc()) {
     $langCapacitacion[]= $fila[$_SESSION['lang']];
 }
+//son 13 las de ivan
 echo '
 	<ul class="sin-estilo">
-                <li class="texto large-grid" style="text-align: center">
+				<li class="texto large-grid">
+					<h2>'.$langCapacitacion[0].'</h2>
+					<p>'.$langCapacitacion[1].'</p>
+					<p>'.$langCapacitacion[2].'</p>
+					<p>'.$langCapacitacion[3].'</p>
+					<p>'.$langCapacitacion[4].'</p>
+					<p>'.$langCapacitacion[5].'</p>
+					<p>'.$langCapacitacion[6].'</p>
+					<p>'.$langCapacitacion[7].'</p>
+					<p>'.$langCapacitacion[8].'</p>
+					<p>'.$langCapacitacion[9].'</p>
+				</li>
+                <li class="texto large-grid">
                     <div class="container">
                         <div class="cabecera">
-                            <h2>'.$langCapacitacion[0].'</h2>
+                            <h2>'.$langCapacitacion[10].'</h2>
                         </div>
 
                         <ul class="cuerpo">
-                            <h3>'.$langCapacitacion[1].'</h3>
+                            <h3>'.$langCapacitacion[11].'</h3>
                             <li>
                                 <ul>
                                     <li>
-                                        <p>'.$langCapacitacion[2].'</p>
+                                        <p>'.$langCapacitacion[12].'</p>
                                     </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[3].'</p>
-                                    </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[4].'</p>
-                                    </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[5].'</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            
-                            <li>
-                                <ul>
-                                    <p>'.$langCapacitacion[6].'</p>
-                                    <li>
-                                        <p>'.$langCapacitacion[7].'</p>
-                                    </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[8].'</p>
-                                    </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[9].'</p>
-                                    </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[10].'</p>
-                                    </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[11].'</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            
-                            <li>
-                                <ul>
-                                    <p>'.$langCapacitacion[12].'</p>
                                     <li>
                                         <p>'.$langCapacitacion[13].'</p>
                                     </li>
@@ -68,20 +45,18 @@ echo '
                                     <li>
                                         <p>'.$langCapacitacion[15].'</p>
                                     </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[16].'</p>
-                                    </li>
+                                </ul>
+                            </li>
+                            
+                            <li>
+                                <ul>
+                                    <p>'.$langCapacitacion[16].'</p>
                                     <li>
                                         <p>'.$langCapacitacion[17].'</p>
                                     </li>
-                                </ul>
-                            </li>
-                        </ul>
-
-                        <ul class="pie">
-                            <h3>'.$langCapacitacion[18].'</h3>
-                            <li>
-                                <ul>
+                                    <li>
+                                        <p>'.$langCapacitacion[18].'</p>
+                                    </li>
                                     <li>
                                         <p>'.$langCapacitacion[19].'</p>
                                     </li>
@@ -91,9 +66,12 @@ echo '
                                     <li>
                                         <p>'.$langCapacitacion[21].'</p>
                                     </li>
-                                    <li>
-                                        <p>'.$langCapacitacion[22].'</p>
-                                    </li>
+                                </ul>
+                            </li>
+                            
+                            <li>
+                                <ul>
+                                    <p>'.$langCapacitacion[22].'</p>
                                     <li>
                                         <p>'.$langCapacitacion[23].'</p>
                                     </li>
@@ -106,9 +84,44 @@ echo '
                                     <li>
                                         <p>'.$langCapacitacion[26].'</p>
                                     </li>
+                                    <li>
+                                        <p>'.$langCapacitacion[27].'</p>
+                                    </li>
                                 </ul>
                             </li>
-                            <p>'.$langCapacitacion[27].'<a href="#" target="_blank">aqui</a> '.$langCapacitacion[28].'</p>
+                        </ul>
+
+                        <ul class="pie">
+                            <h3>'.$langCapacitacion[28].'</h3>
+                            <li>
+                                <ul>
+                                    <li>
+                                        <p>'.$langCapacitacion[29].'</p>
+                                    </li>
+                                    <li>
+                                        <p>'.$langCapacitacion[30].'</p>
+                                    </li>
+                                    <li>
+                                        <p>'.$langCapacitacion[31].'</p>
+                                    </li>
+                                    <li>
+                                        <p>'.$langCapacitacion[32].'</p>
+                                    </li>
+                                    <li>
+                                        <p>'.$langCapacitacion[33].'</p>
+                                    </li>
+                                    <li>
+                                        <p>'.$langCapacitacion[34].'</p>
+                                    </li>
+                                    <li>
+                                        <p>'.$langCapacitacion[35].'</p>
+                                    </li>
+                                    <li>
+                                        <p>'.$langCapacitacion[36].'</p>
+                                    </li>
+                                </ul>
+                            </li>
+                            <p>'.$langCapacitacion[37].'&nbsp<a href="#" target="_blank">'.$langCapacitacion[38].'</a>&nbsp'.$langCapacitacion[39].'</p>
                         </ul>
                     </div>
             	</li><!--#talleres-->
