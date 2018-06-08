cd27fcb17672811bf23bfe4e67d116798b13254a
ravexxgamerxx <35882598+ravexxgamerxx@users.noreply.github.com>
2018-06-08 08:49:56 -0500

    Update Map
    Office map updated

--- objetos/mapa_escuelas.php
+++ objetos/mapa_escuelas.php
@@ -1,4 +1,5 @@
 <?php
-echo '               <div id="map"></div>
+echo '
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1842.6648155205633!2d-103.3482340334054!3d20.697129512555186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b1db17c6df17%3A0x4a428ed953a1621!2sSecretaria+de+Educaci%C3%B3n!5e0!3m2!1ses-419!2smx!4v1528464589454" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 ';
 ?>
